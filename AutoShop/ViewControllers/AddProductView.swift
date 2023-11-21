6e8c0abfca18b34bcb8c864070d80407c6d6c141
Thomas V <thomasvalkenburg1@gmail.com>
2023-11-21 15:22:23 +0100

    Commit

--- AutoShop/ViewControllers/AddProductView.swift
+++ AutoShop/ViewControllers/AddProductView.swift
@@ -45,8 +45,9 @@ class AddProductView: UIViewController {
 
 extension AddProductView: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(filteredProducts![indexPath.row].name!)
-        delegate!.addProduct(product: Cart(product: filteredProducts![indexPath.row]))
+        // Return added product to previous viewcontroller
+        guard let delegate = delegate else { print("Delegate not valid"); return }
+        delegate.addProduct(product: Cart(product: filteredProducts![indexPath.row]))
     }
 }
 
@@ -59,6 +60,7 @@ extension AddProductView: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
+        // Set text for labels
         cell.textLabel?.text = "€\(filteredProducts?[indexPath.row].price ?? 0) - \(filteredProducts?[indexPath.row].name ?? "")"
         cell.detailTextLabel?.text = filteredProducts?[indexPath.row].description ?? ""
         
@@ -69,6 +71,7 @@ extension AddProductView: UITableViewDataSource {
 extension AddProductView: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
+        // Reset filtered products
         filteredProducts = []
         if (searchText == "") {
             filteredProducts = products?.sorted(by: { Product1, Product2 in
@@ -76,24 +79,30 @@ extension AddProductView: UISearchBarDelegate {
             })
         }
         
+        // Filter products
         if let products {
             for product in products {
+                // Check if
                 if (product.name.lowercased().contains(searchText.lowercased()) || product.description.lowercased().contains(searchText.lowercased())) {
                     filteredProducts?.append(product)
                 }
             }
         }
         
+        // Reload tableview
         productTableView.reloadSections([0], with: .automatic)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text = ""
+        // Call text changed function
         self.searchBar(searchBar, textDidChange: searchBar.text ?? "")
+        // Close keyboard
         searchBar.resignFirstResponder()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Close keyboard
         searchBar.resignFirstResponder()
     }
 }
--- AutoShop/ViewControllers/MainViewTable.swift
+++ AutoShop/ViewControllers/MainViewTable.swift
@@ -15,6 +15,9 @@ class Cart{
     }
 }
 
+/// <summary>
+///     Add protocal function
+/// </summary>
 extension MainView: AddProductToCart{
     func addProduct(product : Cart) {
         if (CartList == nil) {
@@ -33,6 +36,9 @@ extension MainView: UITableViewDelegate {
         
     }
     
+    /// <summary>
+    ///     Add preperation for segue to set new VC's delegate to self
+    /// </summary>
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //let destinationVC = segue.destination as! AddProductView
         //destinationVC.delegate = self
@@ -43,7 +49,32 @@ extension MainView: UITableViewDelegate {
     }
 }
 
-extension MainView: UITableViewDataSource {    
+extension MainView: UITableViewDataSource {
+    /// <summary>
+    ///     Add Header for table view
+    /// </summary>
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Winkelmandje"
+    }
+    
+    /// <summary>
+    ///     Add Footer for table view
+    /// </summary>
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        var totalPrice = 0
+        
+        // Sum up prices of all products
+        groupedCartList?.values.forEach({ cart in
+            totalPrice += cart.count * (cart.first?.product.price ?? 0)
+        })
+        
+        // Return total price
+        return "Totaal: €\(totalPrice)"
+    }
+    
+    /// <summary>
+    ///     Select amount of rows needed
+    /// </summary>
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if CartList != nil {
             groupedCartList = Dictionary(grouping: CartList!, by: \.product.id)
@@ -55,43 +86,70 @@ extension MainView: UITableViewDataSource {
             Cart.first!.product.id >= controls!.current_position
         })?.first?.product.id ?? nil
         
-        return groupedCartList?.count ?? 0
+        // Return groupedCartList.count if is bigger than 0, else return 1 (Always return 1 or higher)
+        return groupedCartList?.count ?? 0 >= 1 ? groupedCartList!.count : 1
     }
     
+    /// <summary>
+    ///     Set cells for index
+    /// </summary>
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get cell class
         let cell = cartTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CartTableCell
         
-        let arr = Array(groupedCartList!.values).sorted(by: { $0.first!.product.id < $1.first!.product.id } )
+        // Check if any products in cart list
+        if (groupedCartList != nil && !groupedCartList!.isEmpty) {
+            // Sort products by location (id)
+            let arr = Array(groupedCartList!.values).sorted(by: { $0.first!.product.id < $1.first!.product.id } )
+            
+            // Get product for current index
+            let product: Product = arr[indexPath.row].first!.product
+            
+            // Set text for cell
+            cell.TitleLabel.text = "€\(product.price) - \(product.name)"
+            cell.AmountLabel.text = "\(arr[indexPath.row].count)x - €\(arr[indexPath.row].count * product.price)"
+            
+            // Add product id to button tag
+            cell.MinusButton.tag = product.id
+            cell.PlusButton.tag = product.id
+            
+            // Add targets for + and - button
+            cell.MinusButton.addTarget(nil, action: #selector(minusButtonTapped), for: .touchUpInside)
+            cell.PlusButton.addTarget(nil, action: #selector(plusButtonTapped), for: .touchUpInside)
+            
+            // Make sure buttons aren't hidden
+            cell.MinusButton.isHidden = false
+            cell.PlusButton.isHidden = false
+            cell.AmountLabel.isHidden = false
+            
+            return cell
+        }
         
-        let product: Product = arr[indexPath.row].first!.product
-        
-        // Set text for cell
-        cell.TitleLabel.text = "€\(product.price) - \(product.name)"
-        cell.AmountLabel.text = "\(arr[indexPath.row].count)x - €\(arr[indexPath.row].count * product.price)"
-        
-        // Add product id to button tag
-        cell.MinusButton.tag = product.id
-        cell.PlusButton.tag = product.id
-        
-        // Add targets for + and - button
-        cell.MinusButton.addTarget(nil, action: #selector(minusButtonTapped), for: .touchUpInside)
-        cell.PlusButton.addTarget(nil, action: #selector(plusButtonTapped), for: .touchUpInside)
+        // No products in cart state
+        // Set text and hide buttons
+        cell.TitleLabel.text = "Geen producten geselecteerd."
+        cell.MinusButton.isHidden = true
+        cell.PlusButton.isHidden = true
+        cell.AmountLabel.isHidden = true
         
         return cell
     }
     
     @objc func minusButtonTapped(sender: UIButton) {
         guard let cartIndex = CartList?.firstIndex(where: { $0.product.id == sender.tag }) else { return }
+        // Remove product from list
         CartList?.remove(at: cartIndex)
-        
+    
+        // Reload
         cartTableView.reloadSections([0], with: .automatic)
     }
     
     @objc func plusButtonTapped(sender: UIButton) {
         guard let cart = CartList?.first(where: { $0.product.id == sender.tag }) else { return }
+        // Add product to list
         CartList?.append(cart)
         
+        // Reload
         cartTableView.reloadSections([0], with: .automatic)
     }
 }
